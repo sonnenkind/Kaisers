88031ad2e403c34ceaf2c0184bdaea1d2dbadf69
Martin Kloesch <martin.kloesch@student.tugraz.at>
2011-06-22 08:36:03 +0200

    Zend Framework

---
+++ Kaisers/application/Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('BASE_PATH'))
+{
+  define('BASE_PATH', realpath(dirname(__FILE__) . '/../'));
+}
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+  protected function _initDoctype()
+  {
+ 		$this->bootstrap('view');
+    $view = $this->getResource('view');
+		$view->doctype('XHTML1_STRICT');
+    $view->setEncoding('UTF-8');
+		$view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
+		$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+		$viewRenderer->setView($view);
+		Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+    }
+
+
+}
+
